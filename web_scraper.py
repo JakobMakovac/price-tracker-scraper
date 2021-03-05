07c057c64cea1d4d816ca18192b632879234daf7
Jakob Makovac <jakob.makovac@gmail.com>
2021-03-05 21:40:15 +0100

    Save price info to pricepoints collection

--- web_scraper.py
+++ web_scraper.py
@@ -1,3 +1,4 @@
+import datetime
 from urllib.request import urlopen
 from pymongo import MongoClient
 from pyquery import PyQuery as pq
@@ -6,11 +7,33 @@ client = MongoClient('localhost', 27017)
 
 db = client.priceTrackerDb
 trackers_collection = db.trackermodels
+pricepoints_collection = db.pricepointsmodels
 
 def runScraperAndSave():
-    trackers = trackers_collection.find({})
-    for tracker in trackers:
-        d = pq(url=tracker['url'], opener=lambda url, **kw: urlopen(tracker['url']).read())
-        price = d('.pro-price').text().strip()
-        trackers_collection.update_one({'_id': tracker['_id']}, {'$push': {'pricepoints': price}})
+    urls = getUniqueUrls()
 
+    for _url in urls:
+        now = datetime.datetime.utcnow()
+        d = pq(url=_url, opener=lambda url, **kw: urlopen(_url).read())
+        _price = d('.pro-price').text().strip()
+        pricepoints_collection.update_one(
+            {'url': _url},
+            {
+                '$push': {
+                    'pricepoints': {
+                        'timestamp': now,
+                        'price': _price
+                    }
+                }
+            },
+            upsert=True
+        )
+        
+
+def getUniqueUrls():        
+    trackers = trackers_collection.find({})
+    urls = set()
+    for tracker in trackers:
+        urls.add(tracker['url'])
+    
+    return list(urls)
